6a538f7bdb902ce135858794c9ba8819f0db61dd
monetholt <holtm@oregonstate.edu>
2021-03-14 23:42:47 -0700

    unit test update

--- test/post_test.dart
+++ test/post_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 import 'package:wasteagram/models/post.dart';
 
 void main(){
-  test('Post created from Map should have appropriate property values', (){
+  test('Post created from constructor should have appropriate property values', (){
     final date = DateTime.parse('2021-02-02');
     const url = 'FAKE';
     const quantity = 2;
